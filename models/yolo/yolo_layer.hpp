551c2f50107e73bb7598b0789136d88d421836ea
kartikdutt18 <kartikdutt@live.in>
2020-08-18 21:39:23 +0530

    Start with definition

---
+++ models/yolo/yolo_layer.hpp
@@ -0,0 +1,36 @@
+/**
+ * @file darknet.hpp
+ * @author Kartik Dutt
+ *
+ * Definition of YOLO Layer.
+ *
+ * For more information, kindly refer to the following paper.
+ *
+ * Paper for YOLOv3.
+ * 
+ * @code
+ * @article{Redmon2016,
+ *  author = {Joseph Redmon, Ali Farhadi},
+ *  title = {YOLOv3 :  An Incremental Improvement},
+ *  year = {2019},
+ *  url = {https://pjreddie.com/media/files/papers/YOLOv3.pdf}
+ * }
+ * @endcode
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+
+#ifndef MODELS_YOLO_LAYER_HPP
+#define MODELS_YOLO_LAYER_HPP
+
+
+namespace mlpack {
+namespace ann /** Artificial Neural Network. */{
+
+
+} // namespace ann
+} // namespace mlpack
+#endif
